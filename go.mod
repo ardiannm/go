7322d64774e2bc625adec5af28e1525f809ddac2
Ardian <ardianmaliqaj49@gmail.com>
2025-10-20 20:28:38 +0200

    add models

--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/quic-go/quic-go v0.55.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
+	go.mongodb.org/mongo-driver/v2 v2.3.1
 	go.uber.org/mock v0.6.0 // indirect
 	golang.org/x/arch v0.22.0 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
---
+++ model/movieMode.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Genre struct {
+	GenreID   int    `bson:"genre_id" json:"genre_id"`
+	GenreName string `bson:"genre_name" json:"genre_name"`
+}
+
+type Ranking struct {
+	RankingValue int    `bson:"ranking_value" json:"ranking_value"`
+	RankingName  string `bson:"ranking_name" json:"ranking_name" validate:"required"`
+}
+
+type Movie struct {
+	ID          bson.ObjectID `bson:"_id" json:"_id"`
+	ImdbID      string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
+	Title       string        `bson:"title" json:"title" validate:"required,min=2,max=5"`
+	PosterPath  string        `bson:"poster_path" json:"poster_path" validate:"required,url"`
+	YouTubeID   string        `bson:"youtube_id" json:"youtube_id" validate:"required,dive"`
+	Genre       []Genre       `bson:"genre" json:"genre"`
+	AdminReview string        `bson:"admin_review" json:"admin_review" validate:"required"`
+	Ranking     Ranking       `bson:"ranking" json:"ranking" validate:"required"`
+}

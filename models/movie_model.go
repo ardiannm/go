298bfecf1b874d3c4a9852cc3a0b1062e9560883
Ardian <ardianmaliqaj49@gmail.com>
2025-10-21 21:13:39 +0200

    add .http to test AddMovie route

--- models/movie_model.go
+++ models/movie_model.go
@@ -17,10 +17,10 @@ type Ranking struct {
 type Movie struct {
 	ID          bson.ObjectID `bson:"_id" json:"_id"`
 	ImdbID      string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
-	Title       string        `bson:"title" json:"title" validate:"required,min=2,max=5"`
+	Title       string        `bson:"title" json:"title" validate:"required,min=2,max=500"`
 	PosterPath  string        `bson:"poster_path" json:"poster_path" validate:"required,url"`
-	YouTubeID   string        `bson:"youtube_id" json:"youtube_id" validate:"required,dive"`
-	Genre       []Genre       `bson:"genre" json:"genre"`
+	YouTubeID   string        `bson:"youtube_id" json:"youtube_id" validate:"required"`
+	Genre       []Genre       `bson:"genre" json:"genre" validate:"required,dive"`
 	AdminReview string        `bson:"admin_review" json:"admin_review" validate:"required"`
 	Ranking     Ranking       `bson:"ranking" json:"ranking" validate:"required"`
 }
